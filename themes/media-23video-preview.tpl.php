8f7edc75fd7bf6c8f1e6d96b470bc4a95cf8c4ad
Morten Bartvig <morten@gmail.com>
2015-01-29 13:09:08 +0100

    Added theme for preview of video.

---
+++ themes/media-23video-preview.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ *
+ * Template file for theme('media_23video_oauth').
+ */
+
+?>
+<div class="<?php print $classes; ?> media-23video-oauth-<?php print $id; ?>">
+  <a href="<?php print $url; ?>"><img src="<?php print $thumbnail; ?>"></a>
+</div>
