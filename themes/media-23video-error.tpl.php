2e4cbb4a81315ec6d73df241be049e41ed9ff8d7
Morten Bartvig <bartvig@gmail.com>
2015-02-05 12:52:27 +0100

    Minor change to error template. Add function comments to preprocess hook.

--- themes/media-23video-error.tpl.php
+++ themes/media-23video-error.tpl.php
@@ -8,5 +8,5 @@
 ?>
 <div class="<?php print $classes; ?> media-23video-oauth-<?php print $id; ?>">
   <p><?php print t("Error. Couldn't show video. Contact an administrator."); ?></p>
-  <p><?php print t('Error: @error', array('@error', $message)); ?></p>
+  <p><?php print t('Error: !error', array('!error' => $message)); ?></p>
 </div>
