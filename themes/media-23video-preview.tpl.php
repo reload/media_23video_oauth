30a1a68454b17068a735a83e873fd5f852b83007
Morten Bartvig <bartvig@gmail.com>
2015-02-11 16:42:56 +0100

    Changed template to use the set height and width.

--- themes/media-23video-preview.tpl.php
+++ themes/media-23video-preview.tpl.php
@@ -8,5 +8,5 @@
 
 ?>
 <div class="<?php print $classes; ?> media-23video-oauth-<?php print $id; ?>">
-  <a href="<?php print $url; ?>"><img src="<?php print $thumbnail; ?>"></a>
+  <a href="<?php print $url; ?>"><img src="<?php print $thumbnail; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>"></a>
 </div>
